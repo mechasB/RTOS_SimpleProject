a1858bee019e7c9fa8301c9d25132772cacf9ee4
Michał Błotniak <michalblotniak@gmail.com>
2024-09-30 13:11:59 +0200

    Adding tasks, required libraries and some code for bmp280 and uart

--- RTOS_SimpleProject/Core/Src/freertos.c
+++ RTOS_SimpleProject/Core/Src/freertos.c
@@ -25,7 +25,13 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "bmp280.h"
+#include "i2c.h"
+#include "printf.h"
+#include "usart.h"
+#include "SSD1306_OLED.h"
+#include "GFX_BW.h"
+#include "fonts/fonts.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -47,20 +53,47 @@
 /* USER CODE BEGIN Variables */
 
 /* USER CODE END Variables */
-/* Definitions for defaultTask */
-osThreadId_t defaultTaskHandle;
-const osThreadAttr_t defaultTask_attributes = {
-  .name = "defaultTask",
+/* Definitions for HeartbeatTask */
+osThreadId_t HeartbeatTaskHandle;
+const osThreadAttr_t HeartbeatTask_attributes = {
+  .name = "HeartbeatTask",
   .stack_size = 128 * 4,
   .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for Bmp280Task */
+osThreadId_t Bmp280TaskHandle;
+const osThreadAttr_t Bmp280Task_attributes = {
+  .name = "Bmp280Task",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for OledTask */
+osThreadId_t OledTaskHandle;
+const osThreadAttr_t OledTask_attributes = {
+  .name = "OledTask",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for MutexPrintf */
+osMutexId_t MutexPrintfHandle;
+const osMutexAttr_t MutexPrintf_attributes = {
+  .name = "MutexPrintf"
 };
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
-
+void _putchar(char character)
+{
+  // send char to console etc.
+	osMutexAcquire(MutexPrintfHandle, osWaitForever);
+	HAL_UART_Transmit(&huart2, (uint8_t*)&character, 1, 1000);
+	osMutexRelease(MutexPrintfHandle);
+}
 /* USER CODE END FunctionPrototypes */
 
-void StartDefaultTask(void *argument);
+void StartHeartbeatTask(void *argument);
+void StartBmp280Task(void *argument);
+void StartOledTask(void *argument);
 
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
 
@@ -73,6 +106,9 @@ void MX_FREERTOS_Init(void) {
   /* USER CODE BEGIN Init */
 
   /* USER CODE END Init */
+  /* Create the mutex(es) */
+  /* creation of MutexPrintf */
+  MutexPrintfHandle = osMutexNew(&MutexPrintf_attributes);
 
   /* USER CODE BEGIN RTOS_MUTEX */
   /* add mutexes, ... */
@@ -91,8 +127,14 @@ void MX_FREERTOS_Init(void) {
   /* USER CODE END RTOS_QUEUES */
 
   /* Create the thread(s) */
-  /* creation of defaultTask */
-  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+  /* creation of HeartbeatTask */
+  HeartbeatTaskHandle = osThreadNew(StartHeartbeatTask, NULL, &HeartbeatTask_attributes);
+
+  /* creation of Bmp280Task */
+  Bmp280TaskHandle = osThreadNew(StartBmp280Task, NULL, &Bmp280Task_attributes);
+
+  /* creation of OledTask */
+  OledTaskHandle = osThreadNew(StartOledTask, NULL, &OledTask_attributes);
 
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
@@ -104,22 +146,66 @@ void MX_FREERTOS_Init(void) {
 
 }
 
-/* USER CODE BEGIN Header_StartDefaultTask */
+/* USER CODE BEGIN Header_StartHeartbeatTask */
 /**
-  * @brief  Function implementing the defaultTask thread.
+  * @brief  Function implementing the HeartbeatTask thread.
   * @param  argument: Not used
   * @retval None
   */
-/* USER CODE END Header_StartDefaultTask */
-void StartDefaultTask(void *argument)
+/* USER CODE END Header_StartHeartbeatTask */
+void StartHeartbeatTask(void *argument)
 {
-  /* USER CODE BEGIN StartDefaultTask */
+  /* USER CODE BEGIN StartHeartbeatTask */
+  /* Infinite loop */
+  for(;;)
+  {
+    HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+    osDelay(500);
+  }
+  /* USER CODE END StartHeartbeatTask */
+}
+
+/* USER CODE BEGIN Header_StartBmp280Task */
+/**
+* @brief Function implementing the Bmp280Task thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartBmp280Task */
+void StartBmp280Task(void *argument)
+{
+  /* USER CODE BEGIN StartBmp280Task */
+	BMP280_t Bmp280;
+
+	BMP280_Init(&Bmp280, &hi2c1, 0x76);
+
+	float _Pressure, _Temperature;
+  /* Infinite loop */
+  for(;;)
+  {
+    BMP280_ReadPressureAndTemperature(&Bmp280, &_Pressure, &_Temperature);
+    printf("Temperature: %.2f, Pressure: %.2f\n\r", _Temperature, _Pressure);
+    osDelay(10);
+  }
+  /* USER CODE END StartBmp280Task */
+}
+
+/* USER CODE BEGIN Header_StartOledTask */
+/**
+* @brief Function implementing the OledTask thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartOledTask */
+void StartOledTask(void *argument)
+{
+  /* USER CODE BEGIN StartOledTask */
   /* Infinite loop */
   for(;;)
   {
     osDelay(1);
   }
-  /* USER CODE END StartDefaultTask */
+  /* USER CODE END StartOledTask */
 }
 
 /* Private application code --------------------------------------------------*/
